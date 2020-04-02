4f3bc4ec2a8f45aced53ddfdf6e924c09848c9c4
VMihai-dev <iammihaivisan@gmail.com>
2020-04-02 09:34:30 +0100

    We are counting from 0 :)

--- src/PlayingHand.kt
+++ src/PlayingHand.kt
@@ -1,6 +1,6 @@
 data class PlayingHand (val cards: ArrayList<Card>) {
     fun putCardOnPlayingDeck (deck: DeckOfCards, position: Int){
-        deck.pushCard(cards[position]);
-        cards.removeAt(position);
+        deck.pushCard(cards[position - 1]);
+        cards.removeAt(position - 1);
     }
 }
